a3af0431d03115d1ecc461c7999d2cde72da9d89
Anthonius Munthi <me@itstoni.com>
2023-09-21 18:15:14 +0800

    fix name

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-eval 'gh repo create --public {{ cookiecutter.repository_slug }} -d "{{ cookiecutter.description }}" --disable-wiki'
-
 eval 'git init'
-eval 'git add . -A'
-eval 'pre-commit'
-eval 'pre-commit install'
-eval 'git commit -am "feat: initial commit"'
 eval 'git remote add origin git@github.com:{{ cookiecutter.repository_slug }}.git'
-eval 'git push -u origin main'
-eval 'git push origin main:next'
 
+eval 'gh repo create --public {{ cookiecutter.repository_slug }} -d "{{ cookiecutter.description }}" --disable-wiki'
 {% if cookiecutter.bot_app_id %}
   eval 'gh secret set BOT_APP_ID --body "{{ cookiecutter.bot_app_id }}"'
 {% endif %}
@@ -22,3 +15,10 @@ eval 'git push origin main:next'
 {% if cookiecutter.galaxy_api_key %}
   eval 'gh secret set GALAXY_API_KEY --body "{{ cookiecutter.galaxy_api_key }}"'
 {% endif %}
+
+eval 'git add . -A'
+eval 'pre-commit'
+eval 'pre-commit install'
+eval 'git commit -am "feat: initial commit"'
+eval 'git push -u origin main'
+eval 'git push origin main:next'
