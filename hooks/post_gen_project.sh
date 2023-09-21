c38ffefeb2d19cdb0a56f588f038ce0d0e717821
Anthonius Munthi <me@itstoni.com>
2023-09-21 15:33:43 +0800

    fixing template

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-eval 'gh repo create --public {{ cookiecutter.repository_slug }} -d "{{ cookiecutter.description }}"'
+eval 'gh repo create --public {{ cookiecutter.repository_slug }} -d "{{ cookiecutter.description }}" --disable-wiki'
 
 eval 'git init'
 eval 'git add . -A'
-eval 'git commit -am "initial commit [skip ci]"'
+eval 'git commit -am "feat: initial commit"'
 eval 'git remote add origin git@github.com:{{ cookiecutter.repository_slug }}.git'
 eval 'git push -u origin main'
 
-eval 'gh secret set GALAXY_API_KEY --body "{{ cookiecutter.galaxy_api_key }}"'
+{% if cookiecutter.bot_app_id %}
+  eval 'gh secret set BOT_APP_ID --body "{{ cookiecutter.bot_app_id }}"'
+{% endif %}
+
+{% if cookiecutter.bot_app_private_key %}
+  eval 'gh secret set BOT_APP_PRIVATE_KEY --body "{{ cookiecutter.bot_app_private_key }}"'
+{% endif %}
+
+{% if cookiecutter.galaxy_api_key %}
+  eval 'gh secret set GALAXY_API_KEY --body "{{ cookiecutter.galaxy_api_key }}"'
+{% endif %}
